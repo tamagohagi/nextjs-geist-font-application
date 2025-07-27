e30d61301897315f7bb63b861dcad9e358170a4f
tamagohagi <tamagohagi@gmail.com>
2025-07-27 15:32:17 +0100

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1753626744163

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/src_locales_4b156b77._.js",
+  "static/chunks/node_modules_fb5c6de1._.js",
+  "static/chunks/src_95961904._.js"
+],
+    source: "dynamic"
+});
