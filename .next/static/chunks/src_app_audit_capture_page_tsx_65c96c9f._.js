ba1a8a7e58ebf5a8a25636842f719ed2320a4a22
tamagohagi <tamagohagi@gmail.com>
2025-07-27 15:30:48 +0100

    Add .next/static/chunks/src_app_audit_capture_page_tsx_65c96c9f._.js 1753626654497

---
+++ .next/static/chunks/src_app_audit_capture_page_tsx_65c96c9f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_60155e48._.js",
+  "static/chunks/node_modules_81446ed9._.js"
+],
+    source: "dynamic"
+});
