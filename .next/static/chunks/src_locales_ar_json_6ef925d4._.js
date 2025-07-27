2c73ae50b30e01b29e2fe9bc883fc43e61c7bbf6
tamagohagi <tamagohagi@gmail.com>
2025-07-27 15:32:25 +0100

    Add .next/static/chunks/src_locales_ar_json_6ef925d4._.js 1753626752441

---
+++ .next/static/chunks/src_locales_ar_json_6ef925d4._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_locales_ar_json_faba3f83._.js"
+],
+    source: "dynamic"
+});
